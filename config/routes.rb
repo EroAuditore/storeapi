152f8d07661eff27be37466de930a9b0ab1acbf6
eroauditore <alfa.fenrir@gmail.com>
2021-10-13 16:42:36 -0500

    Initial configuration

---
+++ config/routes.rb
@@ -0,0 +1,17 @@
+Rails.application.routes.draw do
+ 
+  
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  devise_for :users, path: '', path_names: {
+    sign_in: 'login',
+    sign_out: 'logout',
+    registration: 'signup'
+  },
+  controllers: {
+    sessions: 'users/sessions',
+    registrations: 'users/registrations'
+  }
+  get 'private/test'
+  get '/current_user', to: 'current_user#index'
+  get '/member-data', to: 'members#show'
+end
