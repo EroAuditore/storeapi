62ca9ff1ed96f09c0941cfca6dc9ac0741611a8a
eroauditore <alfa.fenrir@gmail.com>
2022-02-12 09:22:48 -0600

    Utc support

--- app/models/sale.rb
+++ app/models/sale.rb
@@ -3,7 +3,7 @@ class Sale < ApplicationRecord
     
     belongs_to :credits, optional: true
     def self.sales_today
-        @sales = self.select("date, total").where(date: Date.today.at_beginning_of_day..Date.today.at_end_of_day).group("date(date)").sum(:total)
+        @sales = self.select("date, total").where(date: Time.zone.today.at_beginning_of_day.utc..Time.zone.today.at_end_of_day.utc).group("date(date)").sum(:total)
         sales_json = []
       
          @sales.each { |sale| sales_json << { date: sale[0], total: sale[1] }  
@@ -14,7 +14,7 @@ class Sale < ApplicationRecord
     end
 
     def self.sales_week
-        @sales = self.select("date, total").where(date: Date.today.beginning_of_week.at_beginning_of_day..Date.today.at_end_of_day).group("date(date)").order("date(date)").sum(:total)
+        @sales = self.select("date, total").where(date: Time.zone.today.beginning_of_week.at_beginning_of_day.utc..Time.zone.today.at_end_of_day.utc).group("date(date)").order("date(date)").sum(:total)
         sales_json = []
       
          @sales.each { |sale| 
@@ -27,7 +27,7 @@ class Sale < ApplicationRecord
     end
     
     def self.sales_month
-        @sales = self.select("date, total").where(date: Date.today.beginning_of_month.at_beginning_of_day..Date.today.at_end_of_day).group("date(date)").sum(:total)
+        @sales = self.select("date, total").where(date: Time.zone.today.beginning_of_month.at_beginning_of_day.utc..Time.zone.today.at_end_of_day.utc).group("date(date)").sum(:total)
         sales_json = []
       
          @sales.each { |sale| 
